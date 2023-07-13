664c13b86e87dd58e0ba9783e4b405d36d546004
Lennart Niecksch <lennart.niecksch@dfki.de>
2023-07-13 11:05:45 +0200

    working now properly with pca for tramline.

--- patchmaps/patchmaps.py
+++ patchmaps/patchmaps.py
@@ -23,7 +23,7 @@ import math
 def get_structure(poly: Polygon, crs='epsg:4326', working_width=36, factor=2, tramline=None) -> gpd.GeoDataFrame:
 
     edge_length = working_width * factor
-    edge_length = 10
+
     if factor % 2 == 0:
         parallel_shift = 4
     else:
@@ -42,21 +42,20 @@ def get_structure(poly: Polygon, crs='epsg:4326', working_width=36, factor=2, tr
     poly = transform(project, poly)
 
 
-
+    centroid =  poly.exterior.centroid
     if tramline is None:
-        coords = np.array(poly.geoms[0].exterior.coords)
+        coords = np.array(poly.exterior.coords)
         pca = PCA(n_components=2)
         pca.fit(coords)
-        # print("JO")
-        # print(type(poly))
-        # print(poly.geoms[0].exterior.centroid)
-        # print(f'''jo {poly.geoms[0].exterior.centroid.x} {poly.geoms[0].exterior.centroid.y}''')
-        p0 = poly.geoms[0].exterior.centroid.x, poly.geoms[0].exterior.centroid.y
-        p1 = p0[0] + 10 * pca.components_[0][0], p0[1] + 10 * pca.components_[0][1]
+        direction = pca.components_[0]
     else:
         tramline = tramline.to_crs('{}'.format(utm))
-        p0 = tramline["geometry"][0].coords[0]  # First coordinate of permanent traffic lane
-        p1 = tramline["geometry"][0].coords[1]
+        p0 = np.array(tramline ["geometry"][0].coords[0])  # First coordinate of permanent traffic laneb
+        p1 = np.array(tramline["geometry"][0].coords[1])
+        direction = p1 - p0
+
+
+
     def unit_vector(vector):
         return vector / np.linalg.norm(vector)
     def angle_between(v1, v2):
@@ -66,9 +65,9 @@ def get_structure(poly: Polygon, crs='epsg:4326', working_width=36, factor=2, tr
 
     x_diff = poly.bounds[2] - poly.bounds[0]
     y_diff = poly.bounds[3] - poly.bounds[1]
-    angle = angle_between(np.array([x_diff, y_diff]), pca.components_[0])
+    angle = angle_between(np.array([x_diff, y_diff]), direction)
 
-    rotated = shapely.affinity.rotate(poly, angle, origin=(p0[0], p0[1]), use_radians=True)
+    rotated = shapely.affinity.rotate(poly, angle, origin=(centroid.x, centroid.y), use_radians=True)
     x_diff = rotated.bounds[2] - rotated.bounds[0]
     y_diff = rotated.bounds[3] - rotated.bounds[1]
 
@@ -86,17 +85,15 @@ def get_structure(poly: Polygon, crs='epsg:4326', working_width=36, factor=2, tr
     def compute_poly(i, j):
         s = so + i * q1 + j * q2
         patch = Polygon([s, s + q1, s + (q1 + q2), s + q2])
-        return shapely.affinity.rotate(patch, -angle, origin=(p0[0], p0[1]), use_radians=True)
+        return shapely.affinity.rotate(patch, -angle, origin=(centroid.x, centroid.y), use_radians=True)
 
     polies = [compute_poly(i, j) for i, j in product(range(0, dimension_a), range(0, dimension_b))]
-    # polies = [compute_poly(0, 0)]
     data = gpd.GeoDataFrame({'geometry': polies})
     data.crs = '{}'.format(utm)
-    # Alternatively use clip and clip to polygon
-    patches_within = data.clip(poly, keep_geom_type=True)
-    # patches_within = data[data.intersects(poly)]
 
-    # transform back to original crs
+    # clip to boundary
+    patches_within = data.clip(poly, keep_geom_type=True)
+
     patches_within = patches_within.to_crs(crs)
 
     return patches_within
